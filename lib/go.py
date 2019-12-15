30c563a1e011649f324ac3da3ac5eceb004d7a43
Peter-Frank Spierenburg <spierepf@hotmail.com>
2019-12-15 17:24:07 +0000

    Added GPS reading code to go.py

--- lib/go.py
+++ lib/go.py
@@ -14,3 +14,35 @@ label.set_text("Hello World!")
 # Load the screen
 
 lv.scr_load(scr)
+
+from machine import UART
+import math
+
+def convert(value):
+  degrees = math.floor(value/100)
+  minutes = value - degrees * 100
+  return degrees + minutes / 60.0
+
+uart = UART(1)
+uart.init(9600,bits=8,parity=None,stop=1,rx=25,tx=26)
+
+line = ''
+while True:
+  while uart.any() == 0:
+    pass
+  try:
+    line += uart.readline().decode('utf-8')
+  except:
+    pass
+  if line.endswith('\r\n'):
+    if line.startswith('$GPGGA'):
+      print(line)
+      data = line.split(',')
+      try:
+        lon = convert(float(data[2]))
+        lat = convert(float(data[4]))
+        dop = float(data[8])
+        print('%.7f'%lon, '%.7f'%lat, dop)
+      except:
+        pass
+    line = ''
